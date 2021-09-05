063a41f91fff46c5e9dd04d0402e6f25c5ec520d
Alexandre Carvalho <alexandrealvesdecarvalho2@gmail.com>
2021-09-05 20:04:07 -0300

    Toolbar and recyclerview
    adicionando toolbar //
    
    - em activity_books.xml
    	-criar WIdget Toolbar:
    	 definir layout_width, layout_height, layout_constraintTop_toTopOf, layout_constraintStart_toStartOf, layout_constraintEnd_toEndOf, background
    
    	*- definir um id do toolbarMain, para aproveitamento de código.
    	adicionar plugin "id 'kotlin-android-extensions'" para funcionar o id em outras classes.
    
    	- criar Widget recyclerview:
    	 para isso adicionar dependencia -->     implementation("androidx.recyclerview:recyclerview:1.2.1")
        	// For control over item selection of both touch and mouse driven selection
        	implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
    
    	<-- depois em activity_books.xml criar widget recyclerview
    	definindo
    
    - mudar o theme conforme necessidade.
     	"app\src\main\res\values\styles.xml"
    	 Apptheme "Theme.AppCompat.NoActionBar"
    	corrigir o fundo preto adicionando um item no theme
    	<item name="android:windowBackground" >@android:color/white</item>

--- app/src/main/java/com/example/bookstore/presentation/books/BooksActivity.kt
+++ app/src/main/java/com/example/bookstore/presentation/books/BooksActivity.kt
@@ -2,11 +2,19 @@ package com.example.bookstore.presentation.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.bookstore.R
+import kotlinx.android.synthetic.main.activity_books.*
+
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.`activity_books`)
+        setContentView(R.layout.activity_books)
+
+        toolbarMain.title = "Books"
+        setSupportActionBar(toolbarMain)
+
+
     }
 }
